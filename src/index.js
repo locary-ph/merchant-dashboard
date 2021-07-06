763c1f6cbafc6c50a8b52d9a289635b4b423f3f7
rafael <dragonwood0@protonmail.com>
2021-07-06 23:05:16 +0800

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter, Route, Switch, Redirect
-} from "react-router-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/css/argon-dashboard-react.min.css";
 import "./index.css";
 
-import AdminLayout from "layouts/Admin.js";
-import AuthLayout from "layouts/Auth.js";
-
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-      <Redirect from="/" to="/admin/index" />
-    </Switch>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
