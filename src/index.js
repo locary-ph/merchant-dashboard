f64aebf53ca3d3d1c595dfd636ccfdf537d72a35
rafael <dragonwood0@protonmail.com>
2021-07-07 16:59:31 +0800

    Removed redux for now

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store";
 
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/css/argon-dashboard-react.min.css";
 import "./index.css";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
