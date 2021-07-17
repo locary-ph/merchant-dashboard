556ae86af59a7e7e95423daf500b8a60309771de
rafael <dragonwood0@protonmail.com>
2021-07-17 22:26:51 +0800

    Add height to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,10 +19,10 @@ import {
 
 const AdminNavbar = (props) => (
     <>
-      <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
+      <Navbar className="navbar-top navbar-dark" id="navbar-main" style={{ height: "4.8rem" }}>
         <Container fluid>
           <span
-            className="h4 mb-0 text-black text-uppercase d-none d-lg-inline-block"
+            className="h4 mb-0 text-black text-uppercase d-inline-block"
           >
             {props.brandText}
           </span>
