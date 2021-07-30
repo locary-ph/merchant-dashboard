5e3f95a24bbd1ac76be08a8e7e64aca2cdffe6eb
rafael <dragonwood0@protonmail.com>
2021-07-30 21:50:21 +0800

    created a separate Stats component

--- src/components/Performance.js
+++ src/components/Performance.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+/**
+ * @format
+ */
+import React from "react";
+import PropTypes from "prop-types";
 
-import {
-  Card,
-  Col,
-  CardTitle,
-  CardHeader,
-  Row,
-  CardBody
-} from "reactstrap";
+import { Card, Col, CardTitle, CardHeader, Row, CardBody } from "reactstrap";
 
-function Performance({  }) {
+const Stats = ({ title, value, subtitle, iconBgColor, icon }) => {
+  return (
+    <Card className="shadow card-stats mb-4 mb-xl-0">
+      <CardBody>
+        <Row>
+          <div className="col">
+            <CardTitle tag="h5" className="text-uppercase text-muted mb-0">
+              {title}
+            </CardTitle>
+            <span className="h2 font-weight-bold mb-0">{value}</span>
+          </div>
+          <Col className="col-auto">
+            <div
+              className={`icon icon-shape text-white rounded-circle shadow ${iconBgColor}`}
+            >
+              <i className={icon} />
+            </div>
+          </Col>
+        </Row>
+        <p className="mt-3 mb-0 text-muted text-sm">
+          <span className="text-nowrap">{subtitle}</span>
+        </p>
+      </CardBody>
+    </Card>
+  );
+};
+
+function Performance() {
   const performanceData = [
-    {title: "Orders", data: "90", subtitle: "Total fo the week"},
-    {title: "Sales", data: "15,000", subtitle: "Total fo the week"}
+    {
+      title: "Orders",
+      value: "90",
+      subtitle: "Total fo the week",
+      iconBgColor: "bg-gradient-info",
+      icon: "ni ni-chart-bar-32",
+    },
+    {
+      title: "Sales",
+      value: "15,000",
+      subtitle: "Total fo the week",
+      iconBgColor: "bg-gradient-orange",
+      icon: "ni ni-bag-17",
+    },
   ];
 
   return (
@@ -21,76 +56,38 @@ function Performance({  }) {
       <CardHeader className="bg-transparent">
         <Row className="align-items-center">
           <div className="col">
-            <h6 className="text-uppercase text-muted ls-1 mb-1">
-              Performance
-            </h6>
+            <h6 className="text-uppercase text-muted ls-1 mb-1">Performance</h6>
             <h2 className="mb-0">Total orders</h2>
           </div>
         </Row>
       </CardHeader>
       <CardBody>
         <Row>
-          <Col>
-            <Card className="shadow card-stats mb-4 mb-xl-0">
-              <CardBody>
-                <Row>
-                  <div className="col">
-                    <CardTitle
-                      tag="h5"
-                      className="text-uppercase text-muted mb-0"
-                    >
-                      Sales
-                    </CardTitle>
-                    <span className="h2 font-weight-bold mb-0">
-                      15,000
-                    </span>
-                  </div>
-                  <Col className="col-auto">
-                    <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                      <i className="fas fa-chart-bar" />
-                    </div>
-                  </Col>
-                </Row>
-                <p className="mt-3 mb-0 text-muted text-sm">
-                  <span className="text-nowrap">Total for the week</span>
-                </p>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="shadow card-stats mb-4 mb-xl-0">
-              <CardBody>
-                <Row>
-                  <div className="col">
-                    <CardTitle
-                      tag="h5"
-                      className="text-uppercase text-muted mb-0"
-                    >
-                      Orders
-                    </CardTitle>
-                    <span className="h2 font-weight-bold mb-0">924</span>
-                  </div>
-                  <Col className="col-auto">
-                    <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
-                      <i className="fas fa-users" />
-                    </div>
-                  </Col>
-                </Row>
-                <p className="mt-3 mb-0 text-muted text-sm">
-                  <span className="text-nowrap">Total for the week</span>
-                </p>
-              </CardBody>
-            </Card>
-          </Col>
+          {performanceData.map((data) => {
+            const { title, value, subtitle, iconBgColor, icon } = data;
+            return (
+              <Col>
+                <Stats {...{ title, value, subtitle, iconBgColor, icon }} />
+              </Col>
+            );
+          })}
         </Row>
       </CardBody>
     </Card>
   );
 }
 
-Performance.defaultProps = {};
+Performance.defaultProps = {
+  iconBgColor: "bg-orange",
+  icon: "ni ni-planet",
+};
 
 Performance.propTypes = {
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  iconBgColor: PropTypes.string,
+  icon: PropTypes.string,
 };
 
 export default Performance;
