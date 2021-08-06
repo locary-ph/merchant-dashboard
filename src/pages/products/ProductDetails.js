38bafc637c6ecd16890bbb6e04777470ae335caa
BoJoNVi03 <bonjovimontes3@gmail.com>
2021-08-06 13:54:23 +0800

    Cleaned some unnecessary codes.

--- src/pages/products/ProductDetails.js
+++ src/pages/products/ProductDetails.js
@@ -3,7 +3,7 @@
  */
 
 import React, { useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
 import {
   Button,
