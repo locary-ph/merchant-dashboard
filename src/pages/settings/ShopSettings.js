421e2cc4b2817664b4b38f46a802cb50cd2d86bc
rafael <dragonwood0@protonmail.com>
2021-08-13 12:30:21 +0800

    only get token from local storage when necessary

---
+++ src/pages/settings/ShopSettings.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ShopSettings({  }) {
+  return (
+    <div />
+  );
+}
+
+ShopSettings.defaultProps = {};
+
+ShopSettings.propTypes = {
+};
+
+export default ShopSettings;
