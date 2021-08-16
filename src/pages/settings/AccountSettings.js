3ea58df9132e28d0e6f4aeed7c8d919e127398b7
rafael <dragonwood0@protonmail.com>
2021-08-16 15:49:41 +0800

    move profile card

--- src/pages/settings/AccountSettings.js
+++ src/pages/settings/AccountSettings.js
@@ -18,6 +18,67 @@ import {
 function AccountSettings() {
   return (
     <>
+      <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
+        <Card className="card-profile shadow">
+          <Row className="justify-content-center">
+            <Col className="order-lg-2" lg="3">
+              <div className="card-profile-image">
+                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                  <img alt="..." className="rounded-circle" />
+                </a>
+              </div>
+            </Col>
+          </Row>
+          {/*
+              <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                <div className="d-flex justify-content-between">
+                  <Button
+                    className="mr-4"
+                    color="info"
+                    href="#pablo"
+                    onClick={(e) => e.preventDefault()}
+                    size="sm"
+                  >
+                    Connect
+                  </Button>
+                  <Button
+                    className="float-right"
+                    color="default"
+                    href="#pablo"
+                    onClick={(e) => e.preventDefault()}
+                    size="sm"
+                  >
+                    Message
+                  </Button>
+                </div>
+              </CardHeader>
+              */}
+          <CardBody className="pt-0 pt-md-4">
+            <Row>
+              <div className="col">
+                <div className="card-profile-stats d-flex justify-content-center mt-7">
+                  <div>
+                    <span className="heading">22</span>
+                    <span className="description">Products</span>
+                  </div>
+                  <div>
+                    <span className="heading">89</span>
+                    <span className="description">Ambot</span>
+                  </div>
+                </div>
+              </div>
+            </Row>
+            <div className="text-center">
+              <h3>Chic Closet</h3>
+              <div className="h5 font-weight-300">
+                <i className="ni location_pin mr-2" />
+                Bucharest, Romania
+              </div>
+              <hr className="my-4" />
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
       <Col className="order-xl-1" xl="8">
         <Card className="bg-secondary shadow">
           <CardHeader className="bg-white border-0">
