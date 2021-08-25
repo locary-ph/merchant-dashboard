49bc37ed4f71434dd6d239266f8ea468ff441162
rafael <dragonwood0@protonmail.com>
2021-08-25 16:04:13 +0800

    Add env variable on ci/cd

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REST_API_BASE_URL || "http://localhost:5000/api/v1",
+  baseURL: process.env.REACT_APP_REST_API_BASE_URL || "http://localhost:5000/api/v1",
 });
 
 const getUserToken = () => localStorage.getItem("token");
