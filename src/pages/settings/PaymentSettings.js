75e5e5c42de5072bed713b074faccae984081849
John Rafael <50954098+jcads@users.noreply.github.com>
2021-08-25 21:56:14 +0800

    Add and edit payment methods. (#87)
    * post request on updating payment methods
    
    * update user on local storage when user state is modified
    
    * synchronously set values for states
    
    * remove user._id in paymentMethod post request

--- src/pages/settings/PaymentSettings.js
+++ src/pages/settings/PaymentSettings.js
@@ -2,7 +2,8 @@
  * @format
  */
 
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useSynchronousState } from "@toolz/use-synchronous-state";
 import {
   Col,
   Button,
@@ -15,21 +16,98 @@ import {
   FormGroup,
 } from "reactstrap";
 
+import LoginContext from "../../contexts/LoginContext";
+import { instance as axios, getUserToken } from "../../axios";
+
 function PaymentSettings() {
+  const { user } = useContext(LoginContext);
+  const [getBankInfo, setBankInfo] = useSynchronousState({});
+  const [getWalletInfo, setWalletInfo] = useSynchronousState({});
+
   // bank transfer inputs state
-  const [bank, setBank] = useState("");
-  const [bankAccNumber, setBankAccNumber] = useState("");
-  const [bankAccName, setBankAccName] = useState("");
-  const [bankInstructions, setBankInstructions] = useState("");
+  const [bank, setBank] = useState();
+  const [bankAccNumber, setAccNum] = useState();
+  const [bankAccName, setAccName] = useState();
+  const [bankInstructions, setBankInstructions] = useState();
 
   // e-wallet inputs state
-  const [ewallet, setEwallet] = useState("");
-  const [ewalletNumber, setEwalletNumber] = useState("");
-  const [ewalletName, setEwalletName] = useState("");
+  const [ewallet, setEwallet] = useState();
+  const [ewalletNumber, setEwalletNumber] = useState();
+  const [ewalletName, setEwalletName] = useState();
 
-  // COD / COP states
-  const [CODInstructions, setCODInstructions] = useState("");
-  const [COPInstructions, setCOPInstructions] = useState("");
+  // Cash on delivery / cash on pickup
+  const [getCOP, setCOP] = useSynchronousState();
+  const [getCOD, setCOD] = useSynchronousState();
+
+  const setValues = (data) => {
+    // THIS IS A VERY BAD IMPLEMENTATION, I'M SO DRAINED RIGHT. NOW WILL FIX THIS LATER.
+    setBankInfo(data.bankTransfer);
+    setWalletInfo(data.eWallet);
+
+    const bankInfo = getBankInfo();
+    const walletInfo = getWalletInfo();
+
+    setBank(bankInfo.bank);
+    setAccNum(bankInfo.accountNumber);
+    setAccName(bankInfo.accountName);
+    setBankInstructions(bankInfo.instructions);
+
+    setEwallet(walletInfo.wallet);
+    setEwalletNumber(walletInfo.accountNumber);
+    setEwalletName(walletInfo.accountName);
+
+    setCOP(data.cashOnPickup);
+    setCOD(data.cashOnDelivery);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          `/merchants/paymentMethod/${user.paymentMethodId}`
+        );
+        // think of other implementations aside this one
+        setValues(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+    // eslint-disable-next-line
+  }, [user]);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${getUserToken()}`,
+      },
+    };
+
+    const data = {
+      bankTransfer: {
+        bank,
+        accountNumber: bankAccNumber,
+        accountName: bankAccName,
+        instructions: bankInstructions,
+      },
+      eWallet: {
+        wallet: ewalletName,
+        accountName: ewalletName,
+        accountNumber: ewalletNumber,
+      },
+      cashOnPickup: getCOP(),
+      cashOnDelivery: getCOD(),
+    };
+
+    try {
+      await axios.post("/merchants/paymentMethod", data, config);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <>
@@ -43,7 +121,7 @@ function PaymentSettings() {
             </Row>
           </CardHeader>
           <CardBody className="bg-secondary px-lg-6">
-            <Form>
+            <Form onSubmit={handleFormSubmit}>
               <div className="inputGroup mb-4">
                 <h2 className="mb-1">Bank transfer</h2>
                 <h5 className="text-muted mb-4">
@@ -82,7 +160,7 @@ function PaymentSettings() {
                           id="accountNumber"
                           type="number"
                           value={bankAccNumber}
-                          onChange={(e) => setBankAccNumber(e.target.value)}
+                          onChange={(e) => setAccNum(e.target.value)}
                         />
                       </FormGroup>
                     </Col>
@@ -101,7 +179,7 @@ function PaymentSettings() {
                           id="accountName"
                           type="text"
                           value={bankAccName}
-                          onChange={(e) => setBankAccName(e.target.value)}
+                          onChange={(e) => setAccName(e.target.value)}
                         />
                       </FormGroup>
                     </Col>
@@ -203,18 +281,15 @@ function PaymentSettings() {
                   <Row>
                     <Col>
                       <FormGroup>
-                        <label
-                          className="form-control-label"
-                          htmlFor="paymentInstructions"
-                        >
+                        <label className="form-control-label" htmlFor="cop">
                           Payment instructions
                         </label>
                         <Input
                           className="form-control-alternative"
-                          id="paymentInstructions"
+                          id="cop"
                           type="text"
-                          value={COPInstructions}
-                          onChange={(e) => setCOPInstructions(e.target.value)}
+                          value={getCOP()}
+                          onChange={(e) => setCOP(e.target.value)}
                         />
                       </FormGroup>
                     </Col>
@@ -232,18 +307,15 @@ function PaymentSettings() {
                   <Row>
                     <Col>
                       <FormGroup>
-                        <label
-                          className="form-control-label"
-                          htmlFor="paymentInstructions"
-                        >
+                        <label className="form-control-label" htmlFor="cod">
                           Payment instructions
                         </label>
                         <Input
                           className="form-control-alternative"
-                          id="paymentInstructions"
+                          id="cod"
                           type="text"
-                          value={CODInstructions}
-                          onChange={(e) => setCODInstructions(e.target.value)}
+                          value={getCOD()}
+                          onChange={(e) => setCOD(e.target.value)}
                         />
                       </FormGroup>
                     </Col>
