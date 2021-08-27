f5311a3b605723113ac5795e179a86913dc7fb42
John Rafael <50954098+jcads@users.noreply.github.com>
2021-08-27 16:17:31 +0800

    add useEffect condition to only render when the user state has been set (#90)

--- src/layouts/Settings.js
+++ src/layouts/Settings.js
@@ -2,32 +2,51 @@
  * @format
  */
 
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { Container, Row } from "reactstrap";
 
 import routes from "../routes";
 
-const Settings = () => (
-  <Container className="mt-6" fluid>
-    <Row>
-      <Switch>
-        {routes.map((prop) => {
-          if (prop.layout === "/admin/settings") {
-            return (
-              <Route
-                path={prop.layout + prop.path}
-                component={prop.component}
-                key={prop.name}
-                exact
-              />
-            );
-          }
-          return null;
-        })}
-      </Switch>
-    </Row>
-  </Container>
-);
+import LoginContext from "../contexts/LoginContext";
+
+const Settings = () => {
+  const { user } = useContext(LoginContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
+  }, [user]);
+
+  return (
+    <Container className="mt-6" fluid>
+      {loading ? (
+        <h1>Loading....</h1>
+      ) : (
+        <Row>
+          <Switch>
+            {routes.map((prop) => {
+              if (prop.layout === "/admin/settings") {
+                return (
+                  <Route
+                    path={prop.layout + prop.path}
+                    component={prop.component}
+                    key={prop.name}
+                    exact
+                  />
+                );
+              }
+              return null;
+            })}
+          </Switch>
+        </Row>
+      )}
+    </Container>
+  );
+};
 
 export default Settings;
