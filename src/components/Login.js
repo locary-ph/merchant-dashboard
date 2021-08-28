226dae80214501aebebf417f8f67292499333d36
John Rafael <50954098+jcads@users.noreply.github.com>
2021-08-28 17:57:17 +0800

    UI: redesign login page (#97)

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,33 +10,14 @@ import LoginForm from "../pages/auth/login/LoginForm";
 const Login = () => (
   <>
     <Col lg="5" md="7">
-      <Card className="bg-secondary shadow border-0">
-        <CardBody className="px-lg-5 py-lg-5">
-          <div className="text-center text-muted mb-4">
-            <small>Welcome back!</small>
-          </div>
+      <Card className="bg-secondary border-0">
+        <CardBody className="px-lg-5 py-lg-0">
           <LoginForm />
         </CardBody>
       </Card>
       <Row className="mt-3">
-        <Col xs="6">
-          <a
-            className="text-light"
-            href="#pablo"
-            onClick={(e) => e.preventDefault()}
-          >
-            <small>Forgot password?</small>
-          </a>
-        </Col>
-        <Col className="text-right" xs="6">
-          <a
-            className="text-light"
-            href="#pablo"
-            onClick={(e) => e.preventDefault()}
-          >
-            <small>Create new account</small>
-          </a>
-        </Col>
+        <Col xs="6" />
+        <Col className="text-right" xs="6" />
       </Row>
     </Col>
   </>
