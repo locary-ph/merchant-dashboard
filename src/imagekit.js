2f27866715aa7f8aee3bac4642f2c58aec956d8c
John Rafael <50954098+jcads@users.noreply.github.com>
2021-08-28 16:22:22 +0800

    fix: `res is not defined` when uploading images (#96)

--- src/imagekit.js
+++ src/imagekit.js
@@ -3,7 +3,7 @@ import ImageKit from "imagekit-javascript";
 const imagekit = new ImageKit({
     publicKey: process.env.REACT_APP_IMAGEKIT_PUBLIC_KEY,
     urlEndpoint: process.env.REACT_APP_IMAGEKIT_URL_ENDPOINT,
-    authenticationEndpoint: "http://localhost:5000/api/v1/imagekit/auth"
+    authenticationEndpoint: `${process.env.REACT_APP_REST_API_BASE_URL}/imagekit/auth`
 });
 
 export default imagekit;
