3d63a94a1d8910fdda4be771c5500654f2039c2b
Bon Jovi Montes <60991513+BoJoNVi@users.noreply.github.com>
2021-09-07 19:31:57 +0800

    Remove "order status tracker" on cancelled orders (#109)

--- src/pages/orders/OrderDetailsStatus.js
+++ src/pages/orders/OrderDetailsStatus.js
@@ -3,6 +3,7 @@
  */
 
 import React from "react";
+import { Badge } from "reactstrap";
 
 export default function OrderDetailsStatus(props) {
   const { order } = props;
@@ -21,39 +22,48 @@ export default function OrderDetailsStatus(props) {
   };
   return (
     <>
-      <h1 className="mb-3">Order Status</h1>
-      <ul className="progressBar pl-0">
-        <li>
-          <div className="d-flex flex-column align-items-center">
-            <div className={checkStatus(0)}>1</div>
-            Order Placed
-          </div>
-        </li>
-        <li>
-          <div className="d-flex flex-column align-items-center">
-            <div className={checkStatus(1)}>2</div>
-            Pending
-          </div>
-        </li>
-        <li>
-          <div className="d-flex flex-column align-items-center">
-            <div className={checkStatus(2)}>3</div>
-            Accepted
-          </div>
-        </li>
-        <li>
-          <div className="d-flex flex-column align-items-center">
-            <div className={checkStatus(3)}>4</div>
-            Dispatched
-          </div>
-        </li>
-        <li>
-          <div className="d-flex flex-column align-items-center">
-            <div className={checkStatus(4)}>5</div>
-            Delivered
-          </div>
-        </li>
-      </ul>
+      <div className="d-flex align-items-center">
+        <h1>Order Status</h1>
+        {orderStatus === 0 ? (
+          <Badge color="danger" className="ml-4">
+            CANCELLED
+          </Badge>
+        ) : null}
+      </div>
+      {orderStatus > 0 ? (
+        <ul className="progressBar pl-0">
+          <li>
+            <div className="d-flex flex-column align-items-center">
+              <div className={checkStatus(0)}>1</div>
+              Order Placed
+            </div>
+          </li>
+          <li>
+            <div className="d-flex flex-column align-items-center">
+              <div className={checkStatus(1)}>2</div>
+              Pending
+            </div>
+          </li>
+          <li>
+            <div className="d-flex flex-column align-items-center">
+              <div className={checkStatus(2)}>3</div>
+              Accepted
+            </div>
+          </li>
+          <li>
+            <div className="d-flex flex-column align-items-center">
+              <div className={checkStatus(3)}>4</div>
+              Dispatched
+            </div>
+          </li>
+          <li>
+            <div className="d-flex flex-column align-items-center">
+              <div className={checkStatus(4)}>5</div>
+              Delivered
+            </div>
+          </li>
+        </ul>
+      ) : null}
     </>
   );
 }
