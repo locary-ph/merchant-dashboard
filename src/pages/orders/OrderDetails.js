ec413e49dff590ee3c825a7edb0a7726c3459e3f
Bon Jovi Montes <60991513+BoJoNVi@users.noreply.github.com>
2021-09-18 20:17:41 +0800

    Implemented an API call for confirmation button in Order Details (#120)
    * Implemented an API call for confirmation
    
    * Reorganized the code a little bit

--- src/pages/orders/OrderDetails.js
+++ src/pages/orders/OrderDetails.js
@@ -13,6 +13,9 @@ import {
   Button,
 } from "reactstrap";
 
+import { instance as axios, getUserToken } from "../../axios";
+
+import toastify from "../../utils/toastify";
 import BackButton from "../../components/BackButton/BackButton";
 import OrderDetailsBuyer from "./OrderDetailsBuyer";
 import OrderDetailsOrder from "./OrderDetailsOrder";
@@ -21,6 +24,22 @@ import OrderDetailsStatus from "./OrderDetailsStatus";
 const OrderDetails = (props) => {
   /* eslint-disable react/destructuring-assignment */
   const { order } = props.location.state;
+
+  const handleConfirm = async (orderID) => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${getUserToken()}`,
+        },
+      };
+      const data = { orderID };
+      await axios.post("/order/confirm", data, config);
+      toastify(4000, "success", "top-right", "Order Confirmed!");
+    } catch (err) {
+      console.error(err);
+      toastify(4000, "error", "top-right", err.response.data.message);
+    }
+  };
 
   return (
     <Container className="mt-5">
@@ -53,7 +72,10 @@ const OrderDetails = (props) => {
             <OrderDetailsStatus order={order} />
             {order.orderStatus !== "CANCELLED" ? (
               <div className="d-flex justify-content-center mt-5">
-                <Button className="theme-border theme-btn theme-active">
+                <Button
+                  className="theme-border theme-btn theme-active"
+                  onClick={() => handleConfirm(order._id)}
+                >
                   Confirm
                 </Button>
               </div>
