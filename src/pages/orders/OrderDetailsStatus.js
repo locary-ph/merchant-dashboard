1700bf533d41993442777d304731fd4b369a093b
Bon Jovi Montes <60991513+BoJoNVi@users.noreply.github.com>
2021-10-04 09:39:55 +0800

    Implemented new design for Order Details (#127)
    * Implemented suggestions for Order Details
    
    * Changed the layout to the new order details
    
    * Fixed lint and format errors
    
    * Added icons to the dropdown menu
    
    * Removed the download invoice button
    
    * Removed unnecessary classnames
    
    * Utilizied text-color bootstrap
    
    * Update naming convention to changeStatus

--- src/pages/orders/OrderDetailsStatus.js
+++ src/pages/orders/OrderDetailsStatus.js
@@ -2,68 +2,120 @@
  * @format
  */
 
-import React from "react";
-import { Badge } from "reactstrap";
+import React, { useState } from "react";
+import {
+  UncontrolledDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  Button,
+} from "reactstrap";
+
+import { instance as axios, getUserToken } from "../../axios";
+
+import toastify from "../../utils/toastify";
 
 export default function OrderDetailsStatus(props) {
   const { order } = props;
+  const [modal, setModal] = useState(false);
+  const [statusMessage, setStatusMessage] = useState("");
+
+  let paymentMethod = "cash";
+  if (order.paymentMethod === "EWALLET" || order.paymentMethod === "BANK") {
+    paymentMethod = "cashless";
+  }
+
   const statusList = {
-    CANCELLED: 0,
-    PENDING: 2,
-    ACCEPTED: 3,
-    DISPATCHED: 4,
-    DELIVERED: 5,
+    cashless: [
+      "UNPAID",
+      "PAID",
+      "TO DELIVER",
+      "ON THE WAY",
+      "DELIVERED",
+      "CANCELLED",
+    ],
+    cashlessIcons: [
+      "fas fa-solid fa-circle text-red mr-2 ",
+      "fas fa-solid fa-money-bill-wave mr-2 ",
+      "fas fa-solid fa-box mr-2 ",
+      "fas fa-solid fa-truck mr-2 ",
+      "fas fa-solid fa-check mr-2 ",
+      "fas fa-times text-red mr-2 ",
+    ],
+    cash: ["PENDING APPROVAL", "APPROVED", "REJECT", "DELIVERED", "CANCELLED"],
+    cashIcons: [
+      "fas fa-solid fa-hourglass-end mr-2 ",
+      "fas fa-solid fa-thumbs-up mr-2 ",
+      "fas fa-solid fa-ban text-red mr-2 ",
+      "fas fa-solid fa-check mr-2 ",
+      "fas fa-times text-red mr-2 ",
+    ],
   };
-  const orderStatus = statusList[order.orderStatus];
-  const checkStatus = (statusNumber) => {
-    if (orderStatus > statusNumber)
-      return "progressBarNumber progressBarActive";
-    return "progressBarNumber";
+  const currentStatusIcon =
+    statusList[`${paymentMethod}Icons`][
+      statusList[paymentMethod].indexOf(order.orderStatus)
+    ];
+
+  const handleStatus = (e) => {
+    setStatusMessage(e.target.textContent);
+    setModal(!modal);
   };
+
+  const changeStatus = async () => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${getUserToken()}`,
+        },
+      };
+      const data = {
+        orderID: order._id,
+        buyerEmail: order.buyerEmail,
+        orderStatus: order.orderStatus,
+      };
+      await axios.post("/orders/status", data, config); // TODO: Need to update the server API soon!
+      toastify(4000, "success", "top-right", "Order Status Changed!");
+      setModal(!modal);
+    } catch (err) {
+      console.error(err);
+      toastify(4000, "error", "top-right", err.response.data.message);
+    }
+  };
+
   return (
     <>
-      <div className="d-flex align-items-center">
-        <h1>Order Status</h1>
-        {orderStatus === 0 ? (
-          <Badge color="danger" className="ml-4">
-            CANCELLED
-          </Badge>
-        ) : null}
-      </div>
-      {orderStatus > 0 ? (
-        <ul className="progressBar pl-0">
-          <li>
-            <div className="d-flex flex-column align-items-center">
-              <div className={checkStatus(0)}>1</div>
-              Order Placed
-            </div>
-          </li>
-          <li>
-            <div className="d-flex flex-column align-items-center">
-              <div className={checkStatus(1)}>2</div>
-              Pending
-            </div>
-          </li>
-          <li>
-            <div className="d-flex flex-column align-items-center">
-              <div className={checkStatus(2)}>3</div>
-              Accepted
-            </div>
-          </li>
-          <li>
-            <div className="d-flex flex-column align-items-center">
-              <div className={checkStatus(3)}>4</div>
-              Dispatched
-            </div>
-          </li>
-          <li>
-            <div className="d-flex flex-column align-items-center">
-              <div className={checkStatus(4)}>5</div>
-              Delivered
-            </div>
-          </li>
-        </ul>
-      ) : null}
+      <UncontrolledDropdown className="order-details-status">
+        <DropdownToggle caret className="text-capitalize order-details-button">
+          <i className={currentStatusIcon} />
+          {order.orderStatus.toLowerCase()}
+        </DropdownToggle>
+        <DropdownMenu right>
+          {statusList[paymentMethod].map((status, index) => (
+            <DropdownItem
+              className="text-capitalize"
+              onClick={(e) => handleStatus(e)}
+            >
+              <i className={statusList[`${paymentMethod}Icons`][index]} />
+              {status.toLowerCase()}
+            </DropdownItem>
+          ))}
+        </DropdownMenu>
+      </UncontrolledDropdown>
+      <Modal isOpen={modal}>
+        <ModalBody>
+          Are you sure you want to set the status to{" "}
+          <b>&quot;{statusMessage.toUpperCase()}&quot;</b>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="danger" onClick={() => setModal(!modal)}>
+            Cancel
+          </Button>
+          <Button onClick={() => changeStatus()}>Confirm</Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 }
