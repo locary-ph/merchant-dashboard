41c3530a0af8d50174d232983181edf2a8bcbc31
jcads <50954098+jcads@users.noreply.github.com>
2021-10-16 15:59:10 +0800

    update inventory with actual products

--- src/pages/home/Inventory.js
+++ src/pages/home/Inventory.js
@@ -1,30 +1,23 @@
-import React from 'react';
-import {
-  Badge,
-  Card,
-  CardHeader,
-  Row,
-  Table
-} from "reactstrap";
+/**
+ * @format
+ */
 
-const products = [
-  {name: "Rose Necklace", qty: 30, sold: 10, isAvailable: true},
-  {name: "Ariel Necklace", qty: 30, sold: 15, isAvailable: true},
-  {name: "Jasmin Necklace", qty: 0, sold: 5, isAvailable: false},
-  {name: "Aurora Necklace", qty: 30, sold: 10, isAvailable: true},
-]
+import React, { useContext } from "react";
+import { Badge, Card, CardHeader, Row, Table } from "reactstrap";
+import LoginContext from "../../contexts/LoginContext";
 
 function Inventory() {
-  const headers = ["Product", "Quantity", "Sold", "Status"];
+  const headers = ["Product", "Quantity", "Status"];
+  const { userInventory } = useContext(LoginContext);
+  const products = userInventory;
+  console.log(products);
 
   return (
     <Card className="shadow">
       <CardHeader className="bg-transparent">
         <Row className="align-items-center">
           <div className="col">
-            <h6 className="text-uppercase text-muted ls-1 mb-1">
-              Products
-            </h6>
+            <h6 className="text-uppercase text-muted ls-1 mb-1">Products</h6>
             <h2 className="mb-0">Inventory</h2>
           </div>
         </Row>
@@ -32,27 +25,32 @@ function Inventory() {
       <Table responsive>
         <thead className="thead-light">
           <tr>
-            {headers.map(header => <th scope="col">{header}</th>)}
+            {headers.map((header) => (
+              <th scope="col">{header}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {products.map(product => (
-              <tr>
-                <th scope="row">
-                  <span className="mb-0 text-sm">
-                    {product.name}
-                  </span>
-                </th>
-                <td>{product.qty}</td>
-                <td>{product.sold}</td>
-                <td>
-                  {product.isAvailable
-                    ? <Badge className="text-uppercase" color="success">in stock</Badge>
-                    : <Badge className="text-uppercase" color="danger">out of stock</Badge>
-                  }
-                </td>
-              </tr>
-            ))}
+          {products.map((product) => (
+            <tr>
+              <th scope="row">
+                <span className="mb-0 text-sm">{product.name}</span>
+              </th>
+              <td>{product.qty}</td>
+              {/* <td>{product.sold}</td> */}
+              <td>
+                {product.qty ? (
+                  <Badge className="text-uppercase" color="success">
+                    in stock
+                  </Badge>
+                ) : (
+                  <Badge className="text-uppercase" color="danger">
+                    out of stock
+                  </Badge>
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </Card>
