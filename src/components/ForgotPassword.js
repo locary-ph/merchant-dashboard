2822333910cdb3bae80568550913d2fb13dc80d0
bojonvi <bonjomontes@gmail.com>
2021-11-05 02:09:31 +0800

    Finalized the front-end for forgot password

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import {
   Alert,
@@ -13,12 +13,33 @@ import {
   InputGroup,
 } from "reactstrap";
 
+import { useParams, useHistory } from 'react-router-dom';
+import { instance as axios } from "../axios";
+
 export default function ForgotPassword() {
+  const [userID, setUserID] = useState("")
   const [newPass, setNewPass] = useState("")
   const [confirmPass, setConfirmPass] = useState("")
   const [strength, setStrength] = useState("");
   const [color, setColor] = useState("danger")
   const [error, setError] = useState("");
+  const { token } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        axios.get(`/auth/forgot-password`, {
+          token,
+        }).then(res => {
+          setUserID(res.data.userID);
+        }) // TODO: have to finalize this
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    validateToken();
+  }, [token])
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -29,7 +50,16 @@ export default function ForgotPassword() {
     } else if (newPass !== confirmPass) {
       setError("Confirm password doesn't match")
     } else {
-      // To do: communicate with server
+      const data = {
+        userID,
+        token,
+        newPass,
+      };
+      try {
+        axios.put("/merchants/change-password", data) // TODO: reconfigure the backend for this
+      } catch (err) {
+        console.log(err);
+      }
     }
   }
 
@@ -76,65 +106,73 @@ export default function ForgotPassword() {
           {error ? <Alert color="danger">
             {error}
           </Alert> : null}
-          <form onSubmit={onSubmit}>
-            <FormGroup className="mb-0">
-              <label htmlFor="new-password">New Password:</label>
-              <InputGroup className="input-group-alternative">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    <i className="ni ni-lock-circle-open" />
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input
-                  id="new-password"
-                  placeholder="Password"
-                  type="password"
-                  autoComplete="new-password"
-                  onChange={onChange}
-                  value={newPass}
-                  required
-                />
-              </InputGroup>
-            </FormGroup>
-            <div className="text-muted font-italic mb-3">
-              {strength ? <small>
-                password strength:
-                <span className={`${color} font-weight-700`}>{strength}</span>
-              </small> : null}
-            </div>
-            <FormGroup className="mb-0">
-              <label htmlFor="confirm-password">Confirm Password:</label>
-              <InputGroup className="input-group-alternative">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    <i className="ni ni-lock-circle-open" />
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input
-                  id="confirm-password"
-                  placeholder="Confirm password"
-                  type="password"
-                  autoComplete="new-password"
-                  onChange={onChange}
-                  value={confirmPass}
-                  required
-                />
-              </InputGroup>
-            </FormGroup>
-            <div className="text-muted font-italic mb-2">
-              {confirmPass && confirmPass !== newPass ? <small className="font-weight-700 text-danger">
-                Password doesn&apos;t match!
-              </small> : null}
-            </div>
+          {userID ?
+            <form onSubmit={onSubmit}>
+              <FormGroup className="mb-0">
+                <label htmlFor="new-password">New Password:</label>
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-lock-circle-open" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    id="new-password"
+                    placeholder="Password"
+                    type="password"
+                    autoComplete="new-password"
+                    onChange={onChange}
+                    value={newPass}
+                    required
+                  />
+                </InputGroup>
+              </FormGroup>
+              <div className="text-muted font-italic mb-3">
+                {strength ? <small>
+                  password strength:
+                  <span className={`${color} font-weight-700`}>{strength}</span>
+                </small> : null}
+              </div>
+              <FormGroup className="mb-0">
+                <label htmlFor="confirm-password">Confirm Password:</label>
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-lock-circle-open" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    id="confirm-password"
+                    placeholder="Confirm password"
+                    type="password"
+                    autoComplete="new-password"
+                    onChange={onChange}
+                    value={confirmPass}
+                    required
+                  />
+                </InputGroup>
+              </FormGroup>
+              <div className="text-muted font-italic mb-2">
+                {confirmPass && confirmPass !== newPass ? <small className="font-weight-700 text-danger">
+                  Password doesn&apos;t match!
+                </small> : null}
+              </div>
+              <div className="text-center">
+                <Button className="mt-4" color="danger">
+                  Cancel
+                </Button>
+                <Button type="submit" className="mt-4" color="primary">
+                  Change Password
+                </Button>
+              </div>
+            </form> :
             <div className="text-center">
-              <Button className="mt-4" color="danger">
-                Cancel
+              <h2>Error</h2>
+              <p>It seems that you are trying to access an expired link.</p>
+              <Button onClick={() => {history.push("/auth/login")}} className="mt-4">
+                Go back to login
               </Button>
-              <Button type="submit" className="mt-4" color="primary">
-                Change Password
-              </Button>
-            </div>
-          </form>
+            </div>}
         </CardBody>
       </Card>
     </div>
